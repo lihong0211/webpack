89fb3639de568a0824b83f2727510561ba5cba9a
lihong <lihong@bilibili.com>
2021-04-15 08:40:02 +0800

    init

--- 01-05 浅析 Webpack 打包输出内容/lesson/dist/test.js
+++ 01-05 浅析 Webpack 打包输出内容/lesson/dist/test.js
@@ -4,7 +4,7 @@
 /******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-/******/
+/******/    debugger
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
@@ -83,51 +83,25 @@
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
 /******/ })
-/************************************************************************/
-/******/ ({
-
+({
 /***/ "./src/content.js":
-/*!************************!*\
-  !*** ./src/content.js ***!
-  \************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
+(function(module, exports) {
 eval("function Content() {\n\tvar dom = document.getElementById('root');\n\tvar content = document.createElement('div');\n\tcontent.innerText = 'content';\n\tdom.append(content);\n}\n\nmodule.exports = Content;\n\n//# sourceURL=webpack:///./src/content.js?");
-
 /***/ }),
 
 /***/ "./src/header.js":
-/*!***********************!*\
-  !*** ./src/header.js ***!
-  \***********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
+(function(module, exports) {
 eval("function Header() {\n\tvar dom = document.getElementById('root');\n\tvar header = document.createElement('div');\n\theader.innerText = 'header';\n\tdom.append(header);\n}\n\nmodule.exports = Header;\n\n//# sourceURL=webpack:///./src/header.js?");
-
 /***/ }),
 
 /***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
+(function(module, exports, __webpack_require__) {
 eval("var Header = __webpack_require__(/*! ./header.js */ \"./src/header.js\");\nvar Sidebar = __webpack_require__(/*! ./sidebar.js */ \"./src/sidebar.js\");\nvar Content = __webpack_require__(/*! ./content.js */ \"./src/content.js\");\n\nnew Header();\nnew Sidebar();\nnew Content();\n\n//# sourceURL=webpack:///./src/index.js?");
-
 /***/ }),
 
 /***/ "./src/sidebar.js":
-/*!************************!*\
-  !*** ./src/sidebar.js ***!
-  \************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
+(function(module, exports) {
 eval("function Sidebar() {\n\tvar dom = document.getElementById('root');\n\tvar sidebar = document.createElement('div');\n\tsidebar.innerText = 'sidebar';\n\tdom.append(sidebar);\n}\n\nmodule.exports = Sidebar;\n\n//# sourceURL=webpack:///./src/sidebar.js?");
-
 /***/ })
 
 /******/ });
